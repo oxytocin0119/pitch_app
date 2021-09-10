c3aa9aa79ada3c50246a6d1deeb1fc01db8cef5d
oxytocin0119 <oxytocin0119@gmail.com>
2021-09-10 09:18:32 +0900

    add own delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+#  before_action :admin_user,      only: :destroy
+  before_action :current_or_admin, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -73,9 +74,19 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    @user = current_user
+    destroy_user = User.find(params[:id])
+    if @user.admin? && @user != destroy_user
+      destroy_user.destroy
+      flash[:success] = "アカウントを消去しました"
+      redirect_to users_url
+    elsif !(@user.admin?) && @user == destroy_user
+      destroy_user.destroy
+      flash[:success] = "アカウントを消去しました"
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
   end
 
   private
@@ -105,6 +116,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def current_or_admin
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user.admin? || current_user?(@user)
     end
 
 end
